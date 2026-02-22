4af723e8020d5c090f6dba43a67cb6941065a474
arsumaehae <arsumaehae@gmail.com>
2026-02-22 16:28:31 +0700

    feature : custome users model with roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    # กำหนด Role ให้ระบบ
+    ROLE_CHOICES = (
+        ('member', 'Member (นักศึกษา)'),
+        ('librarian', 'Librarian (บรรณารักษ์)'),
+    )
+    
+    role = models.CharField(
+        max_length=20, 
+        choices=ROLE_CHOICES, 
+        default='member',
+        verbose_name="ตำแหน่ง"
+    )
+
+    def is_librarian(self):
+        return self.role == 'librarian'
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
