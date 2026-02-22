25439ff4a9937d2850dc750a63e598026459783c
arsumaehae <arsumaehae@gmail.com>
2026-02-22 23:50:13 +0700

    main : style login register page

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,29 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class StudentSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+# เปลี่ยนจากการสืบทอด UserCreationForm มาใช้ ModelForm ธรรมดา
+class StudentSignUpForm(forms.ModelForm):
+    # สร้างช่องกรอกรหัสผ่านแบบง่ายๆ ไม่ต้องมีกฎเกณฑ์
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput()
+    )
+
+    class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name') # ให้กรอกแค่นี้พอ
+        fields = ('username', 'first_name', 'last_name') # ช่องที่บังคับให้กรอก
 
     def save(self, commit=True):
+        # 1. จำลองการเซฟข้อมูลมาไว้ในตัวแปร user ก่อน
         user = super().save(commit=False)
-        user.role = 'member'  # 🔒 ล็อกสิทธิ์! สมัครเองเป็นได้แค่นักศึกษา
+        
+        # 2. เอารหัสผ่านที่พิมพ์มาเข้ารหัส (Hash) ให้ระบบอ่านเข้าใจ
+        user.set_password(self.cleaned_data['password'])
+        
+        # 3. ล็อกสิทธิ์เป็นนักศึกษา (Member) เหมือนเดิม
+        user.role = 'member'
+        
+        # 4. บันทึกลง Database ของจริง
         if commit:
             user.save()
         return user
